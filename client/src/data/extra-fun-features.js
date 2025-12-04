e16f2ae0b64cc462ac9c8d3dd180b15ce5bb451d
Ethan Tran <tran.ethan.000@gmail.com>
2025-12-03 23:21:19 -0800

    V.1.2.6 New data added for extra fun features page

--- client/src/data/extra-fun-features.js
+++ client/src/data/extra-fun-features.js
@@ -1,11 +1,14 @@
 export const extraFunFeatures = [
     {
-        id: 'example-project',
-        title: 'Example Fun Project',
-        description: 'A fun project I built in my spare time to explore new ideas and experiment with creative concepts',
+        id: 'ginkgo leaves',
+        title: 'Vibe coded leaf shadows',
+        description: 'I was walking back to my dorms at night and I saw the pixelated shadows of ginkgo leaves. I wanted to simulate something through programming and this is my first attempt at it.',
         coverImage: '/assets/extras/example-thumb.jpg',
-        technologies: ['JavaScript', 'HTML', 'CSS'],
-        link: 'https://example.com',
+        technologies: ['React', 'Javascript', 'Tailwind'],
+        link: 'https://github.com/ET5008/Leaf-light-shadow',
         type: 'experiment'
+    },
+    {
+        
     }
 ]
