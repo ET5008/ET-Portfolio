a6eae1a3a6b26344c7c7c2a5cf009a706e7eea8e
Ethan Tran <tran.ethan.000@gmail.com>
2025-12-10 23:30:08 -0800

    Revert "V.1.2.6 New data added for extra fun features page"
    This reverts commit e16f2ae0b64cc462ac9c8d3dd180b15ce5bb451d.

--- client/src/data/extra-fun-features.js
+++ client/src/data/extra-fun-features.js
@@ -1,14 +1,11 @@
 export const extraFunFeatures = [
     {
-        id: 'ginkgo leaves',
-        title: 'Vibe coded leaf shadows',
-        description: 'I was walking back to my dorms at night and I saw the pixelated shadows of ginkgo leaves. I wanted to simulate something through programming and this is my first attempt at it.',
+        id: 'example-project',
+        title: 'Example Fun Project',
+        description: 'A fun project I built in my spare time to explore new ideas and experiment with creative concepts',
         coverImage: '/assets/extras/example-thumb.jpg',
-        technologies: ['React', 'Javascript', 'Tailwind'],
-        link: 'https://github.com/ET5008/Leaf-light-shadow',
+        technologies: ['JavaScript', 'HTML', 'CSS'],
+        link: 'https://example.com',
         type: 'experiment'
-    },
-    {
-        
     }
 ]
